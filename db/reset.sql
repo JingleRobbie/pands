f3ba89663abcbee375d408bc89377a94a90516ef
Robert Schell <schell.robert.dale2017@gmail.com>
2026-05-11 20:51:56 -0500

    update reset.sql to include cut-down

--- db/reset.sql
+++ db/reset.sql
@@ -5,19 +5,22 @@ USE pands;
 
 SET FOREIGN_KEY_CHECKS = 0;
 
-TRUNCATE TABLE shipment_lines;
-TRUNCATE TABLE shipments;
-TRUNCATE TABLE inventory_transactions;
-TRUNCATE TABLE inventory_counts;
-TRUNCATE TABLE production_runs;
-TRUNCATE TABLE production_run_groups;
-TRUNCATE TABLE wo_accessories;
 TRUNCATE TABLE contacts;
 TRUNCATE TABLE customer_addresses;
-TRUNCATE TABLE work_order_lines;
-TRUNCATE TABLE work_orders;
+TRUNCATE TABLE customers;
+TRUNCATE TABLE cut_down_groups;
+TRUNCATE TABLE cut_downs;
+TRUNCATE TABLE inventory_counts;
+TRUNCATE TABLE inventory_transactions;
+TRUNCATE TABLE production_run_groups;
+TRUNCATE TABLE production_runs;
 TRUNCATE TABLE purchase_order_lines;
 TRUNCATE TABLE purchase_orders;
-TRUNCATE TABLE customers;
+TRUNCATE TABLE shipment_lines;
+TRUNCATE TABLE shipments;
+truncate table wip_ledger;
+TRUNCATE TABLE wo_accessories;
+TRUNCATE TABLE work_order_lines;
+TRUNCATE TABLE work_orders;
 
 SET FOREIGN_KEY_CHECKS = 1;
